1c95c156a6a6d030a4557059c42ee62dcc005040
caesarakalaeii <caesarlp@gmx.de>
2023-08-14 20:58:05 +0200

    swappers now WITH GUNS

--- Swap.cs
+++ Swap.cs
@@ -11,7 +11,6 @@ public class Swap : GameMode
 
     public override Task<OnPlayerSpawnArguments> OnPlayerSpawning(MyPlayer player, OnPlayerSpawnArguments request)
     {
-        player.SetLightGadget("Pickaxe", 0, true);
         player.SetRunningSpeedMultiplier(1.25f);
         player.SetFallDamageMultiplier(0f);
         player.SetJumpMultiplier(1.5f);
