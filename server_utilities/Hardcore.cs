330b8bf68be66236001fe6be25d0365f673f02d9
caesarakalaeii <caesarlp@gmx.de>
2023-08-14 22:51:44 +0200

    no more hit markers in hardcore

--- server_utilities/Hardcore.cs
+++ server_utilities/Hardcore.cs
@@ -9,10 +9,12 @@ public class Hardcore : GameMode
 
     public override Task OnPlayerSpawned(MyPlayer player)
     {
+        ServerSettings.HitMarkersEnabled = false;
         player.SetRunningSpeedMultiplier(1.25f);
         player.SetFallDamageMultiplier(2f);
         player.SetHP(50);
         player.SetGiveDamageMultiplier(2f);
+
         return base.OnPlayerSpawned(player);
     }
 }
