7f8814adc19d1d8a3cca0a9c92924000f93dd6c9
caesarakalaeii <caesarlp@gmx.de>
2023-08-14 22:46:31 +0200

    now without god mode

--- server_utilities/Hardcore.cs
+++ server_utilities/Hardcore.cs
@@ -13,7 +13,6 @@ public class Hardcore : GameMode
         player.SetFallDamageMultiplier(2f);
         player.SetHP(50);
         player.SetGiveDamageMultiplier(2f);
-        if (player.SteamID == 76561198053896127) player.SetReceiveDamageMultiplier(0f);
         return base.OnPlayerSpawned(player);
     }
 }
